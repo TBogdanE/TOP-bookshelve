dd4a6fcecf90a641e0d322ffcbe0fa709fb30169
Toma Bogdan <tbogdane@TBE>
2023-07-14 18:03:35 +0300

    user can now add books to main section

--- book.js
+++ book.js
@@ -3,6 +3,7 @@ let overlayBox = document.getElementById('overlay');
 let newBookBtn = document.getElementById('add-new-book-btn');
 let overlayAddBtn = document.getElementById('overlay-card-add-book-btn')
 let overlayCancelBtn = document.getElementById('overlay-card-cancel-book-btn');
+let main = document.querySelector('main');
 
 //INITIALISE VARIABLES
 
@@ -15,11 +16,19 @@ overlayCancelBtn.addEventListener('click', overlayToggle);
 
 document.addEventListener('keydown', function (event) {
     if (event.key === 'Escape') {
-        if(checkOverflowStatus() === 'flex') {
-        overlayToggle();
+        if (checkOverflowStatus() === 'flex') {
+            overlayToggle();
+        }
+    } else if (event.key === 'Enter') {
+        if (checkOverflowStatus() === 'flex') {
+            if (event.target.tagName !== 'INPUT') {
+                submitOverlayBook();
+                event.preventDefault();
+            }
         }
     }
 })
+
 
 //FUNCTIONS
 
@@ -28,6 +37,7 @@ function checkOverflowStatus() {
     let status = window.getComputedStyle(overlayBox).display;
     return status;
 }
+
 //shows or hides the overlay
 function overlayToggle() {
     if (checkOverflowStatus() === 'none') {
@@ -39,9 +49,9 @@ function overlayToggle() {
 }
 
 //clears the input boxes in the overlay menu
-function overlayClearInputs () {
+function overlayClearInputs() {
     document.getElementById('overlay-title').value = '';
-    document.getElementById('overlay-author').value ='';
+    document.getElementById('overlay-author').value = '';
     document.getElementById('overlay-pages').value = '';
 }
 
@@ -50,11 +60,64 @@ function submitOverlayBook() {
     let bookInfo = {
         title: document.getElementById('overlay-title').value,
         author: document.getElementById('overlay-author').value,
-        pages: document.getElementById('overlay-pages').value,
+        page: document.getElementById('overlay-pages').value,
         date: new Date()
     }
     bookCollection.push(bookInfo);
-    console.log(bookCollection);
-    console.log(bookInfo);
     overlayToggle();
+    createBookCard(bookInfo);
 }
+
+// creates the book card with the info given
+function createBookCard(book) {
+    let card = document.createElement('div');
+    card.setAttribute('class', 'card-book-box');
+
+    let cardTextBox = document.createElement('div');
+    cardTextBox.setAttribute('class', 'card-book-box-text');
+
+    let cardTitle = document.createElement('div');
+    cardTitle.setAttribute('class', 'card-title');
+    cardTitle.textContent = book.title;
+
+    let cardAuthor = document.createElement('div')
+    cardAuthor.setAttribute('class', 'card-author');
+    cardAuthor.textContent = book.author;
+
+    let cardPages = document.createElement('div');
+    cardPages.setAttribute('class', 'card-pages');
+    cardPages.textContent = book.page;
+
+    let cardDate = document.createElement('div');
+    cardDate.setAttribute('class', 'card-date');
+
+    let cardButtons = document.createElement('div');
+    cardButtons.setAttribute('class', 'card-book-box-buttons');
+
+    let readBtn = document.createElement('button');
+    readBtn.textContent = 'Read';
+    readBtn.setAttribute('class', 'read-book-btn');
+    readBtn.setAttribute('type', 'submit');
+
+    let deleteBtn = document.createElement('button');
+    deleteBtn.setAttribute('class', 'delete-book-btn');
+    deleteBtn.textContent = 'Delete';
+
+    main.appendChild(card);
+    card.appendChild(cardTextBox);
+    card.appendChild(cardButtons);
+    cardTextBox.appendChild(cardTitle);
+    cardTextBox.appendChild(cardAuthor);
+    cardTextBox.appendChild(cardPages);
+    cardTextBox.appendChild(cardDate);
+    cardButtons.appendChild(readBtn);
+    cardButtons.appendChild(deleteBtn);
+}
+
+/*
+NEXT:
+- delete button on overlay card
+- fix bug with enter key on overlay card
+- make the info panel work
+
+*/
